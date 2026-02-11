ff8db95a64059f36630f187b52b1b9d43aff6bc3
Reno Faza Givaro <renogivaro@gmail.com>
2026-02-11 21:50:53 +0700

    feat add set passwors student & verify email

--- Backend/src/models/auth.model.ts
+++ Backend/src/models/auth.model.ts
@@ -5,3 +5,4 @@
   HEADER = 'header',
   JSON = 'json'
 }
+
---
+++ Backend/src/services/index.ts
@@ -0,0 +1,5 @@
+import TokenServices from './tokens.service';
+import StudentServices from './students.service';
+import AuthServices from './auth.service';
+
+export { TokenServices, StudentServices, AuthServices }
